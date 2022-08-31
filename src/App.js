8455a188b0ab1508e7900b6f103517e496081ea0
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-08-31 23:01:54 +0100

    Initial Commit(s) - Deleted react follow come files - Added the App, Card and Form files both JS and CSS - Added the design images folder - Included the some files in the .gitignore file - wrote some JSX in the App file and linked it to the index.js file - Commit DONE.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // The values of the state can be altered/changed as required.
+    this.state = {
+      cardName: null,
+      cardNumber: 0,
+      month: 'MM',
+      year: 'YY',
+      cvc: 0,
+    };
+  }
+  render() {
+    return <div className="App">App</div>;
+  }
 }
 
 export default App;
