34e366c4649c52f2cb60769d355051bd1b136552
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-09-04 17:21:17 +0100

    .

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,52 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: false, number: false, time: false, cvc: false };
+    this.state = {
+      name: false,
+      number: false,
+      time: false,
+      cvc: false,
+      code: true,
+    };
+    // console.log(this.spaceCardNumber('12345678901234567890'));
+    this.inputRef = React.createRef();
+    this.specialChar = /[`!@#$%^&*()_+\-=\\[\]{};':"\\|,.<>\\/?~]/;
+    this.numChar = /[0-9]/;
+    this.letterChar = /[a-z]/;
   }
+
+  componentDidMount() {
+    this.inputRef.current.addEventListener('keydown', this.handleBackspace);
+  }
+
+  componentWillUnmount() {
+    this.inputRef.current.removeEventListener('keydown', this.handleBackspace);
+  }
+
+  handleBackspace = (e) => {
+    if (e.keyCode === 8) this.setState({ code: false });
+    else this.setState({ code: true });
+  };
+
+  spaceCardNumber = (str) => {
+    for (let i = 0; i <= str.length; i++)
+      if (this.state.code)
+        if (str.length === 4 + i * 5 && str.length < 19) str += ' ';
+    return str;
+  };
+
+  // using Regular expression (RegExp)
+  checkName(name) {
+    return this.specialChar.test(name) || this.numChar.test(name);
+  }
+
+  checkNumber(number) {
+    return this.letterChar.test(number) || this.specialChar.test(number);
+  }
+
+  getCardNumber = (e) => {
+    this.props.setCardNumber(this.spaceCardNumber(e.target.value));
+  };
 
   validateName = () => {
     if (this.props.cardName)
@@ -57,8 +101,15 @@ class Form extends React.Component {
 
           <div className="form-group">
             <label>CARD NUMBER</label>
-            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
-            <span className={`error ${this.state.name ? 'show' : ''}`}>
+            <input
+              ref={this.inputRef}
+              type="text"
+              maxLength={19}
+              value={this.props.cardNumber}
+              onChange={this.getCardNumber}
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
+            <span className={`error ${this.state.number ? 'show' : ''}`}>
               Wrong format, numbers only
             </span>
           </div>
@@ -67,16 +118,8 @@ class Form extends React.Component {
             <div className="form-group-joint">
               <label>EXP.DATE(MM/YY)</label>
               <div className="form-group-joint-input">
-                <input
-                  type="number"
-                  placeholder="MM"
-                  onChange={(e) => this.props.setMonth(e.target.value)}
-                />
-                <input
-                  type="number"
-                  placeholder="YY"
-                  onChange={(e) => this.props.setYear(e.target.value)}
-                />
+                <input type="number" placeholder="MM" />
+                <input type="number" placeholder="YY" />
               </div>
               <span className={`error ${this.state.time ? 'show' : ''}`}>
                 Can't be blank
@@ -85,11 +128,7 @@ class Form extends React.Component {
 
             <div className="form-group-disjointed-input">
               <label>CVC</label>
-              <input
-                type="tel"
-                placeholder="e.g. 123"
-                onChange={(e) => this.props.setCvc(e.target.value)}
-              />
+              <input type="tel" placeholder="e.g. 123" />
               <span className={`error ${this.state.cvc ? 'show' : ''}`}>
                 Can't be blank
               </span>
