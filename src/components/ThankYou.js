e603f5a0410f4e74d63d9e10d1eef96142a3a244
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-09-06 06:29:30 +0100

    style thankyou page

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,14 +1,16 @@
-import './ThankYou.css';
-import React from 'react';
+import "./ThankYou.css";
+import iconComplete from "../img/icon-complete.svg";
+import React from "react";
 
 class ThankYou extends React.Component {
   render() {
     return (
-      <div className={`Thankyou ${this.props.form ? 'hide' : ''}`}>
-        THANK YOU!
-        <p>We've added your card details</p>
-        <div className="btn btn-submit">
-            <input type="submit" value="Continue" />
+      <div className={`thankyou ${this.props.form ? "hide" : ""}`}>
+        <img src={iconComplete} className="icon-complete" />
+        <span className="thank-you--title">THANK YOU!</span>
+        <p className="thank-you--text">We've added your card details</p>
+        <div className="btn btn-submit thank-you--btn">
+          <input type="submit" value="Continue" />
         </div>
       </div>
     );
