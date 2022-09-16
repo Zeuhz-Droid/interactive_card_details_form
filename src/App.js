032c773d2079f08c94253d3f976755cb076ced7d
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-09-16 08:21:32 +0100

    Refactor <App /> to use function based component Change App to function component and update the state to make it easier to pass values around

--- src/App.js
+++ src/App.js
@@ -1,80 +1,46 @@
-import './App.css';
-import React from 'react';
-import Card from './components/Card';
-import Form from './components/Form';
-import ThankYou from './components/ThankYou';
+import "./App.css";
+import React, { useState } from "react";
+import Card from "./components/Card";
+import Form from "./components/Form";
+import ThankYou from "./components/ThankYou";
 
-class App extends React.Component {
-  // The values of the state can be altered/changed as required.
-  state = {
-    cardName: '',
-    cardNumber: '',
-    month: '',
-    year: '',
-    cvc: '',
-    form: true,
+const App = () => {
+  // The values of the cardDetails can be altered/changed as required.
+  const [cardDetails, setCardDetails] = useState({
+    cardName: { id: "name", value: "" },
+    cardNumber: { id: "number", value: "" },
+    month: { id: "month", value: "" },
+    year: { id: "year", value: "" },
+    cvc: { id: "cvc", value: "" },
+  });
+  const [form, setForm] = useState(true);
+
+  const handleStateChange = (value) => {
+    setCardDetails((prevDetails) => {
+      return { ...prevDetails, cardName: { id: "name", value: value } };
+    });
   };
 
-  setCardName = (value) => {
-    this.setState({ cardName: value });
+  const setFormValue = () => {
+    setForm((prevFormValue) => !prevFormValue);
   };
 
-  setCardNumber = (value) => {
-    this.setState({ cardNumber: value });
-  };
-
-  setMonth = (value) => {
-    this.setState({ month: value });
-  };
-
-  setYear = (value) => {
-    this.setState({ year: value });
-  };
-
-  setCvc = (value) => {
-    this.setState({ cvc: value });
-  };
-
-  setForm = (value) => {
-    this.setState({ form: value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Card
-          cardName={this.state.cardName}
-          cardNumber={this.state.cardNumber}
-          month={this.state.month}
-          year={this.state.year}
-          cvc={this.state.cvc}
-        />
-        <Form
-          setCardName={this.setCardName}
-          setCardNumber={this.setCardNumber}
-          setMonth={this.setMonth}
-          setYear={this.setYear}
-          setCvc={this.setCvc}
-          setForm={this.setForm}
-          cardNumber={this.state.cardNumber}
-          cardName={this.state.cardName}
-          month={this.state.month}
-          year={this.state.year}
-          cvc={this.state.cvc}
-          form={this.state.form}
-        />
-        <ThankYou
-          setCardName={this.setCardName}
-          setCardNumber={this.setCardNumber}
-          setMonth={this.setMonth}
-          setYear={this.setYear}
-          setCvc={this.setCvc}
-          form={this.state.form}
-          setForm={this.setForm}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Card cardDetails={cardDetails} />
+      <Form
+        handleChange={handleStateChange}
+        setForm={setFormValue}
+        cardDetails={cardDetails}
+        form={form}
+      />
+      <ThankYou
+        handleChange={handleStateChange}
+        form={form}
+        setForm={setForm}
+      />
+    </div>
+  );
+};
 
 export default App;
